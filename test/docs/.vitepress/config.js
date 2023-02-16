74733091d95bbe91dbf066edd15a22e24ad85a5f
manchan <manchan4869@163.com>
2023-02-16 21:23:48 +0800

    remove test/docs

--- test/docs/.vitepress/config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitepress";
-
-import compression from "../../../dist/esm/index.mjs";
-
-export default defineConfig({
-  async buildEnd() {
-    compression({ inDir: "./docs/.vitepress/dist" });
-  },
-});
